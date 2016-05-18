934192cffc3bc1a45f3b9a87f35c15f61eea4aa5
HecticKaluha <stefanoverhoeve44@gmail.com>
2016-05-18 16:14:55 +0200

    opdracht 1 2 en 3

--- Week11/Persistence JPA Entitymanager/bank/src/test/java/util/tests.java
+++ Week11/Persistence JPA Entitymanager/bank/src/test/java/util/tests.java
@@ -24,6 +24,7 @@ public class tests
 {
     private EntityManagerFactory emf = Persistence.createEntityManagerFactory("bankPU");
     private EntityManager em = emf.createEntityManager(); 
+    private DatabaseCleaner dbc = new DatabaseCleaner(em);
     
     public tests() {
         
@@ -45,20 +46,83 @@ public class tests
     
     @After
     public void tearDown() {
+        try
+        {
+            dbc.clean();
+        }
+        catch(Exception ex)
+        {
+            
+        }
     }
-    
     @Test
     public void vraag1()
     {
         Account account = new Account(111L);
         em.getTransaction().begin();
+        //klaarmaken voor database
         em.persist(account);
         //TODO: verklaar en pas eventueel aan
+        //return true omdat het object nog niet naar de database is geschreven, dat gebeurd pas bij de commit
         assertNull(account.getId());
+        //naar de database
         em.getTransaction().commit();
+        //returned "AccountId: 1"
         System.out.println("AccountId: " + account.getId());
         //TODO: verklaar en pas eventueel aan
+        //1based dus return true
         assertTrue(account.getId() > 0L);
+        //returned "AccountId: 1"
+        /*Create new table Account
+        (
+            Id Number Primary Key,
+            accountNr Number Unique,
+            balance number,
+            threshold number
+        );
+        
+        Insert into Account(Id, accountNr, balance, threshold) 
+        Values(1, 111, 0, 0);
+        
+        eindresultaat = tabel account aangemaakt met een row met de gegevens : 1, 111, 0, 0
+        */
+    }
+    
+    @Test
+    public void vraag2()
+    {
+        //new account maken
+        Account account = new Account(111L);
+        em.getTransaction().begin();
+        //account klaarmaken voor database
+        em.persist(account);
+        //return true omdat er geen record in de database bekend is (geen commit)
+        assertNull(account.getId());
+        //er wordt nooit een commit uitgevoerd, onderstaande clear local cashe
+        em.getTransaction().rollback();
+        // TODO code om te testen dat table account geen records bevat. Hint: bestudeer/gebruik AccountDAOJPAImpl
+        //geen sqlqueries geenv erandering in database
+    }
+    
+    @Test
+    public void vraag3()
+    {
+        Long expected = -100L;
+        //nieuw account met accountNr 111
+        Account account = new Account(111L);
+        //we passen in het lokaal bekende account de id aan naar -100
+        account.setId(expected);
+        em.getTransaction().begin();
+        //we maken de lokaal bekende account object klaar voor de database
+        em.persist(account);
+        //TODO: verklaar en pas eventueel aan
+        assertNotEquals(expected, account.getId());
+        //de database wordt geupdate (possibly lege kopie)
+        em.flush();
+        //TODO: verklaar en pas eventueel aan
+        //assertEquals(expected, account.getId();
+        em.getTransaction().commit();
+        //TODO: verklaar en pas eventueel aan
     }
 
 
--- Week11/Persistence JPA Entitymanager/bank/target/generated-sources/annotations/bank/domain/Account_.java
+++ Week11/Persistence JPA Entitymanager/bank/target/generated-sources/annotations/bank/domain/Account_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.3.v20160428-rNA", date="2016-05-18T14:02:45")
+@Generated(value="EclipseLink-2.6.3.v20160428-rNA", date="2016-05-18T15:44:11")
 @StaticMetamodel(Account.class)
 public class Account_ { 
 
